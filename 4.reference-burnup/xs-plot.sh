b651ae8fd6fd7dee3c16f4c2dfdec15207bcc84e
R. Vignolo <rvignolo@tecna.com>
2016-11-15 09:38:17 -0300

    = por defecto que grafique en gnuplot

--- 4.reference-burnup/xs-plot.sh
+++ 4.reference-burnup/xs-plot.sh
@@ -16,23 +16,32 @@ xsnamestex=(\$D_{1}\$~[cm] \
             \$\\Sigma_{f1}\$~[cm\$^{-1}\$] \
             \$\\Sigma_{f2}\$~[cm\$^{-1}\$])
 
-for i in `seq 0 ${nxs}`; do
-  m4 -Dfile=${xsnames[$i]} \
-     -Dcoef=${xsnamestex[$i]} \
-     -Dindex=`echo "$i + 2" |bc` \
-     -Dstyle1=`echo "$i + 2" |bc` xs-burn-ppl.m4 > pyxplot.ppl
-  
-  printf "  plotting ${xsnames[$i]}..."
-  pyxplot pyxplot.ppl 
-  printf "ok!\n"
-done
-rm pyxplot.ppl
-echo "joining pdfs"
-for i in `seq 1 ${nxs}`; do
-  pdfjoin --rotateoversize 'false' ${xsnames[$i-1]}.pdf ${xsnames[$i]}.pdf 2> /dev/null 
-  rm ${xsnames[$i-1]}.pdf
-  mv ${xsnames[$i]}-joined.pdf ${xsnames[$i]}.pdf 
-done
-mv ${xsnames[$i]}.pdf xs-vs-burn.pdf
+# si tenes pyxplot descomenta esto
+# for i in `seq 0 ${nxs}`; do
+#   m4 -Dfile=${xsnames[$i]} \
+#      -Dcoef=${xsnamestex[$i]} \
+#      -Dindex=`echo "$i + 2" |bc` \
+#      -Dstyle1=`echo "$i + 2" |bc` xs-burn-ppl.m4 > pyxplot.ppl
+#   
+#   printf "  plotting ${xsnames[$i]}..."
+#   pyxplot pyxplot.ppl 
+#   printf "ok!\n"
+# done
+# rm pyxplot.ppl
+# echo "joining pdfs"
+# for i in `seq 1 ${nxs}`; do
+#   pdfjoin --rotateoversize 'false' ${xsnames[$i-1]}.pdf ${xsnames[$i]}.pdf 2> /dev/null 
+#   rm ${xsnames[$i-1]}.pdf
+#   mv ${xsnames[$i]}-joined.pdf ${xsnames[$i]}.pdf 
+# done
+# mv ${xsnames[$i]}.pdf xs-vs-burn.pdf
+# 
+# okular xs-vs-burn.pdf &
 
-okular xs-vs-burn.pdf &
+
+# si tenes gnuplot, usa esto
+j=1
+for i in ${xsnames[@]}; do
+  j=$((j+1))
+ gnuplot -p -e "set xlabel 'burnup'; set ylabel '${i}'; plot 'xs.dat' u 1:${j} w lp pt ${j} ti '${i}'" 
+done
